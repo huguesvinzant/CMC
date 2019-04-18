011b86cf5d3efca9820c3f3b71e30e95fc9dcfa4
lbruhin <lena.bruhin@epfl.ch>
2019-04-18 15:02:38 +0200

    what we did during the exercises

--- Project3/Webots/controllers/pythonController/network.py
+++ Project3/Webots/controllers/pythonController/network.py
@@ -8,12 +8,24 @@ from solvers import euler, rk4
 
 def phases_ode(time, phases, freqs, coupling_weights, phases_desired):
     """Network phases ODE"""
-    return np.zeros_like(phases)
-
+    
+    dphase = np.zeros_like(phases_desired)
+    
+    for i, phase_i in enumerate(phases):
+        sums = 0
+        for j, phase_j in enumerate(phases):
+            sums += coupling_weights[i][j]*np.sin(phase_j-phase_i-phases_desired[i][j])
+        dphase[i] = 2*np.pi*freqs[i] + sums
+    
+    return dphase
 
 def amplitudes_ode(time, amplitudes, rate, amplitudes_desired):
     """Network amplitudes ODE"""
-    return np.zeros_like(amplitudes)
+    amps = np.zeros_like(amplitudes)
+    for i, amp in enumerate(amplitudes):
+        amps[i] = rate[i]*(amplitudes_desired[i]-amplitudes[j])
+    
+    return amps
 
 
 def motor_output(phases_left, phases_right, amplitudes_left, amplitudes_right):
@@ -55,20 +67,25 @@ class PhaseEquation(ODESolver):
         super(PhaseEquation, self).__init__(phases_ode, timestep, euler)
         self.n_joints = 10
         self.phases = 1e-4*np.random.ranf(2*self.n_joints)
-        self.freqs = np.zeros(2*self.n_joints)
+        self.freqs = freqs
         self.coupling_weights = np.zeros([2*self.n_joints, 2*self.n_joints])
         self.phases_desired = np.zeros([2*self.n_joints, 2*self.n_joints])
         self.set_parameters(freqs, phase_lag)
 
     def set_parameters(self, freqs, phase_lag):
         """Set parameters of the network"""
+       
 
         # Set coupling weights
         pylog.warning("Coupling weights must be set")
+        self.coupling_weights = 10*np.ones_like([2*self.n_joints, 2*self.n_joints])
 
         # Set desired phases
         pylog.warning("Desired phases must be set")
-
+        self.phases_desired = phase_lag
+         
+        self.freqs =2*np.pi*freqs
+         
     def step(self):
         """Step"""
         self.phases += self.integrate(
@@ -93,13 +110,15 @@ class AmplitudeEquation(ODESolver):
 
     def set_parameters(self, amplitudes, turn):
         """Set parameters of the network"""
+        
 
         # Set convergence rates
         pylog.warning("Convergence rates must be set")
+        self.rates = ((2*self.n_joints)**2)/8
 
         # Set desired amplitudes
         pylog.warning("Desired amplitudes must be set")
-
+        
     def step(self):
         """Step"""
         self.amplitudes += self.integrate(
