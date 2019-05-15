f9b327471a19cfd5a0dd1792baebbd214acd32b9
lbruhin <lena.bruhin@epfl.ch>
2019-05-15 16:10:12 +0200

    changes in 9b and 9c

--- Project3/Webots/controllers/pythonController/exercise_9b.py
+++ Project3/Webots/controllers/pythonController/exercise_9b.py
@@ -5,47 +5,60 @@ from run_simulation import run_simulation
 from simulation_parameters import SimulationParameters
 import cmc_pylog as pylog
 import matplotlib.pyplot as plt
-
+from tempfile import TemporaryFile
 
 def exercise_9b(world, timestep, reset):
     """Exercise 9b"""
+    
+    outfile = TemporaryFile()
+    
     parameter_set = [[
         SimulationParameters(
-            simulation_duration=10,
+            simulation_duration=20,
             drive=4.5,
             amplitude_gradient = [1,1],
             phase_lag=phi,
             amp_factor = amp,
             # ...
         )
-        for phi in [ 2*np.pi/(3*10),2*np.pi/10, 3*2*np.pi/10]]
-        for amp in np.linspace(0.1,5,25)]
+        for phi in np.array([ 2*np.pi/(3*10), 2*np.pi/(2*10),2*np.pi/10,2*2*np.pi/10, 3*2*np.pi/10])]
+        for amp in np.linspace(0.1,5.0,25)]
     
     pylog.warning(np.shape(parameter_set))
-    phi = [ 2*np.pi/(3*10),2*np.pi/10, 3*2*np.pi/10]
-    amp = np.linspace(0.1,5,25)
-    energy = np.zeros((len(phi), len(amp)))
-    for i in range(0 ,len(phi)):
-        for j in range(0,len(amp)):
+    phi = np.array([ 2*np.pi/(3*10), 2*np.pi/(2*10),2*np.pi/10,2*2*np.pi/10, 3*2*np.pi/100])
+    amp = np.linspace(0.1,5.0,25)
+    energy = np.zeros_like(parameter_set)
+    for ind, angle in enumerate(phi):
+        for j in range(0, len(amp)):
+            pylog.warning('angle index '+str(ind))
+            pylog.warning('amp index '+str(j) + ' ampfactor used: ' + str(amp[j]))
             reset.reset()
-            parameters = parameter_set[i][j]
+            parameters = parameter_set[j][ind]
             run_simulation(
                     world,
                     parameters,
                     timestep,
                     int(1000*parameters.simulation_duration/timestep),
-                    logs="./logs/simulation9b_phi{}_amp{}.npz".format(i,amp[j])
+                    logs="./logs/simulation9b_phi{}_amp{}.npz".format(ind,j)
                     )
 
-            data = np.load("llogs/simulation9b_phi{}_amp{}.npz".format(i,amp[j]))
-            velocity = data["joints"][:,:,1]
+            data = np.load("logs/simulation9b_phi{}_amp{}.npz".format(ind,j))
+            velocity = np.diff(data["joints"][:,:,0], axis = 0)/timestep
+            velocity = np.insert(velocity,0,0,axis = 0)
             torque = data["joints"][:,:,2]
-            energy[i,j] = np.mean(np.trapz(velocity*torque,dx = timestep))
+            energy[j,ind] = np.mean(np.trapz(velocity*torque,dx = timestep))
+            pylog.info('Energy: '+ str(energy[j,ind]))
         
-    plt.imshow(energy, extent = [2*np.pi/(3*10),6*np.pi/10,0.1,5])
+    np.save(outfile, energy)
+
+    #outfile.seek(0) #for when you want to open it again
+    plt.imshow(energy, extent = [0.1,5,2*3.14/(3*10),6*3.14/10])
     plt.xlabel('phase lag')
     plt.ylabel('amplitude factor')
     plt.title('Energy estimation')
     plt.colorbar()
     plt.show()
+    with open('energy9b.txt','wb') as f:
+        for line in energy:
+            np.savetxt(f, line, fmt='%.2f')
 
--- Project3/Webots/controllers/pythonController/exercise_9c.py
+++ Project3/Webots/controllers/pythonController/exercise_9c.py
@@ -5,29 +5,30 @@ import matplotlib.pyplot as plt
 from run_simulation import run_simulation
 from simulation_parameters import SimulationParameters
 import cmc_pylog as pylog
+from tempfile import TemporaryFile
 
 
 
 def exercise_9c(world, timestep, reset):
     """Exercise 9c"""
     # Parameters
-    n_joints = 10
+    outfile = TemporaryFile()
 
     parameter_set = [[
         SimulationParameters(
-            simulation_duration=10,
+            simulation_duration=20,
             drive=4.5,
             amplitude_gradient = [head, tail],
             phase_lag=2*np.pi/10,
             # ...
         )
-        for head in np.linspace(0.1,1.0,5)]
-        for tail in np.linspace(0.1,1.0,5)]
+        for head in np.linspace(0.1,1.5,15)]
+        for tail in np.linspace(0.1,1.5,15)]
         # for amplitudes in ...
     
     pylog.warning(np.shape(parameter_set))
-    head = np.linspace(0.1,1.0,5)
-    tail = np.linspace(0.1,1.0,5)
+    head = np.linspace(0.1,1.5,15)
+    tail = np.linspace(0.1,1.5,15)
     pylog.warning(head)
     energy = np.zeros((len(head), len(tail)))
     for i in range(0 ,len(head)):
@@ -39,15 +40,18 @@ def exercise_9c(world, timestep, reset):
                     parameters,
                     timestep,
                     int(1000*parameters.simulation_duration/timestep),
-                    logs="./logs/simulation9c_head{}_tail{}.npz".format(head[i],tail[j])
+                    logs="./logs/simulation9c_more_head{}_tail{}.npz".format(head[i],tail[j])
                     )
 
-            data = np.load("logs/simulation9c_head{}_tail{}.npz".format(head[i],tail[j]))
+            data = np.load("logs/simulation9c_more_head{}_tail{}.npz".format(head[i],tail[j]))
             velocity = np.diff(data["joints"][:,:,0], axis = 0)/timestep
             velocity = np.insert(velocity,0,0,axis = 0)
             torque = data["joints"][:,:,2]
             energy[i,j] = np.mean(np.trapz(velocity*torque,dx = timestep))
-        
+      
+    np.save(outfile, energy)
+    outfile.seek(0) #for when you want to open it again
+
     plt.imshow(energy, extent = [0.1,1,0.1,1])
     plt.xlabel('head factor')
     plt.ylabel('tail factor')
