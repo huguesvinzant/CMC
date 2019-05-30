878beb4f566c13dacc614639a2bb282426785635
LudovicaR <ludovica.romanin@epfl.ch>
2019-05-30 18:51:37 +0200

    Update 9g

--- Project3/Webots/controllers/pythonController/exercise_9g.py
+++ Project3/Webots/controllers/pythonController/exercise_9g.py
@@ -1,6 +1,7 @@
 """Exercise 9g"""
 
 import numpy as np
+import matplotlib.pyplot as plt 
 from run_simulation import run_simulation
 from simulation_parameters import SimulationParameters
 
@@ -10,10 +11,10 @@ def exercise_9g(world, timestep, reset):
     
     parameters = SimulationParameters(
             simulation_duration=60, 
-            amplitude_gradient = [0.2,1], 
+            amplitude_gradient = [1,1], 
             phase_lag=2*np.pi/10, 
             turn=[1, 'Right'],
-            offset = np.pi, #optimal phase lag
+#            offset = 2*np.pi/10, #optimal phase lag
             amp_factor = 1.0, 
             drive_mlr = 1.5
             ) 
@@ -22,6 +23,7 @@ def exercise_9g(world, timestep, reset):
                 parameters,
                 timestep,
                 int(1000*parameters.simulation_duration/timestep),
-                logs="./logs/simulation9g.npz"
+                logs="logs/simulation9g.npz"
             )
+    
 
